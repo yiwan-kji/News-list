c5890dac4176c20459ceead42c937063769872a7
Ricemonster <rm766140773@163.com>
2020-04-01 21:01:54 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,68 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    active: 0,
+    page: 1,
+    limit: 10,
+    data: {},
+  },
+  // 生命周期
+  onLoad: function () {
+    wx.stopPullDownRefresh()
+    let that = this
+    // 网络请求
+    wx.request({
+      url: `${app.globalData.path}/news`,
+      method: 'POST',
+      data: {
+           type: "all",
+           page: that.data.page,
+           limit: that.data.limit
+      },
+      success: function (res) {
+        let changeData = {}
+        if(res.data.length === 0){
+          wx.showToast({
+            title: '数据已经是最新啦！',
+            icon: 'none',
+            duration: 3000
+          })
+        }else{
+          for(let i=0;i<res.data.length;i++){
+            res.data[i].typeText = that.typeFunction(res.data[i].type)
+            changeData[i] = res.data[i]
+          }
+          that.setData({
+            data: changeData
+          })
+        }
+      },
+     fail:function(err){
+          console.log('数据请求错误!')
+     }
+  })
+  },
+  // 事件
+  typeFunction(id){
+    let map = {2:"农业资讯",3:"价格行情",4:"农药新闻",5:"化肥新闻"}
+    return map[id]
+  },
+  // 上拉刷新
+  onPullDownRefresh: function () {
+    let that = this;
+    that.setData({
+      page: that.data.page+1
+    })
+    this.onLoad();
+  },
+  // 跳转事件
+  Todetail(event) {
+   let item = event.currentTarget.dataset.gid
+   let data = JSON.stringify(item)
+   wx.navigateTo({
+     url: '/pages/detal/detal?goods=' + data
+   })
+  }
+})
